42ee54a9c0dd4d0e7f5eaaad174b979a9acc364a
Om Bharama Dev <ombharamadev@proton.me>
2024-08-12 04:54:47 +0000

    day 1 complete

---
+++ day1/p2.py
@@ -0,0 +1,5 @@
+
+name = "aryan jaswal"
+age =21
+
+print("Hi ," +str(name) + "and you are "+str(age)+ " year old")
---
+++ day1/p3.py
@@ -0,0 +1,7 @@
+num = int(input("Enter yoru Number "))
+if num >0:
+    print("Positive Number")
+elif num == 0:
+    print("Zero number")
+else:
+    print("Negative")
