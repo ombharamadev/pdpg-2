b158ef8a63bd3cbf555506417769f546c9e82508
Om Bharama Dev <ombharamadev@proton.me>
2024-08-14 04:43:27 +0000

    added day 3

---
+++ day3/arr_1.py
@@ -0,0 +1,12 @@
+
+
+arr_data = []
+
+for i in range(1,100):
+    arr_data.append(i)
+
+###print hte arrray
+#print(arr_data)
+
+for api in arr_data:
+    print(api)
---
+++ day3/json_cr.py
@@ -0,0 +1,14 @@
+
+import json
+
+data = [{
+    "name":"aryan",
+    "age":"21"
+},
+{
+    "name":"ram",
+    "age":"51"
+}]
+
+for api in data:
+    print(api["name"])
